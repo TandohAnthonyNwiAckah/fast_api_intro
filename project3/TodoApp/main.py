7566188c029d9bf3e338026fc1a45ba2358bb218
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2024-10-10 11:25:10 +0000

    added : put request for todos

--- project3/TodoApp/main.py
+++ project3/TodoApp/main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -18,7 +19,9 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
+
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
@@ -37,9 +40,11 @@ class TodoRequest(BaseModel):
         }
     }
 
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
 
 @app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
@@ -48,9 +53,28 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found')
 
+
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
 
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency,
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)
+                      ):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        todo_model.title = todo_request.title
+        todo_model.description = todo_request.description
+        todo_model.priority = todo_request.priority
+        todo_model.complete = todo_request.complete
+
+        db.add(todo_model)
+        db.commit()
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found')
