fbc012b7786bd17898453856ae0b38d42d83fad2
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2024-10-10 11:00:17 +0000

    added : post request for todos

--- project3/TodoApp/main.py
+++ project3/TodoApp/main.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import Session
 import models
 from database import engine, SessionLocal
 from models import Todos
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-
 
 def get_db():
     db = SessionLocal()
@@ -18,9 +18,24 @@ def get_db():
     finally:
         db.close()
 
-
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=5)
+    complete: bool
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Todo for today",
+                "description": "Doings for today",
+                "priority": 2,
+                "complete": False,
+            }
+        }
+    }
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
@@ -33,3 +48,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found')
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
