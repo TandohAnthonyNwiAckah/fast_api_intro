dcc1a81ac0f6281cfae03ee9225dfe6cb5b884cf
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2024-10-11 09:40:15 +0000

    added : auth user

--- project3/TodoApp/routers/auth.py
+++ project3/TodoApp/routers/auth.py
@@ -1,8 +1,42 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from starlette import status
 
 router = APIRouter()
 
 
-@router.get("/auth")
-async def get_user():
-    return {"user": "authenticated"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "username": "JohnDoe",
+                "email": "john.doe@email.com",
+                "first_name": "John",
+                "last_name": "Doe",
+                "password": "password123",
+                "role": "admin"
+            }
+        }
+    }
+
+
+@router.post("/auth", status_code=status.HTTP_200_OK)
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+    return create_user_model
